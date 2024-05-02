bcf9164325cac4a2b78b12e169f3e415f8a9a956
APPLE <apple@APPLEs-MacBook-Air-2.local>
2024-05-02 11:58:50 +0530

    new

--- src/component/Purchasing.js
+++ src/component/Purchasing.js
@@ -14,17 +14,17 @@ const Purchasing = () => {
 <br />
 
 <ul className='pur-list'>
-  <li>1. You the customer understand that Mobile Care is selling this device to you in "AS IS" conditions.
+  <li>➛ You the customer understand that Mobile Care is selling this device to you in "AS IS" conditions.
  </li><br />
-  <li>2. Devices come certified pre-owned with original parts, unless specified by Mobile Care otherwise.
+  <li>➛ Devices come certified pre-owned with original parts, unless specified by Mobile Care otherwise.
 </li><br />
-<li>3 . All purchased devices come with a 30 Day warranty from Mobile Care.</li><br />
-<li>4. If you the customer damage the device, then Mobile Care is not responsible, but we can offer you a discount on repairs!</li><br />
-<li>5. All devices come carrier unlock!</li><br />
-<li>6. Device Unlock information is available upon request.</li><br />
-<li>7. Mobile Care is not responsible for carrier services that are not provided by us!</li><br />
-<li>8. Device purchasing by Debit/Credit Card will be subject to a ID verification and signature!</li><br />
-<li>9. Devices sold by cash will be checked for illegal tender!</li>
+<li>➛ All purchased devices come with a 30 Day warranty from Mobile Care.</li><br />
+<li>➛ If you the customer damage the device, then Mobile Care is not responsible, but we can offer you a discount on repairs!</li><br />
+<li>➛ All devices come carrier unlock!</li><br />
+<li>➛ Device Unlock information is available upon request.</li><br />
+<li>➛ Mobile Care is not responsible for carrier services that are not provided by us!</li><br />
+<li>➛ Device purchasing by Debit/Credit Card will be subject to a ID verification and signature!</li><br />
+<li>➛ Devices sold by cash will be checked for illegal tender!</li>
 </ul>
 <br />
 
